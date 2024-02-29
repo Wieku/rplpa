92c9f6553ea1f0ed7bae926b53bc89e3cd62d534
Sebastian Krajewski <mail@wieku.me>
2024-03-01 00:25:33 +0100

    Add LazerHitResult enum

--- consts.go
+++ consts.go
@@ -2,17 +2,40 @@ package rplpa
 
 // All osu playmodes
 const (
-	OSU   = 0
-	TAIKO = 1
-	CTB   = 2
-	MANIA = 3
+	OSU = iota
+	TAIKO
+	CTB
+	MANIA
 )
 
 // ClickState
 const (
-	LEFTCLICK  = 1 << 0
-	RIGHTCLICK = 1 << 1
-	KEY1       = 1 << 2
-	KEY2       = 1 << 3
-	SMOKE      = 1 << 4
+	LEFTCLICK = 1 << iota
+	RIGHTCLICK
+	KEY1
+	KEY2
+	SMOKE
+)
+
+type LazerHitResult string
+
+const (
+	LazerNone                LazerHitResult = "none"
+	LazerMiss                LazerHitResult = "miss"
+	LazerMeh                 LazerHitResult = "meh"
+	LazerOk                  LazerHitResult = "ok"
+	LazerGood                LazerHitResult = "good"
+	LazerGreat               LazerHitResult = "great"
+	LazerPerfect             LazerHitResult = "perfect"
+	LazerSmallTickMiss       LazerHitResult = "small_tick_miss"
+	LazerSmallTickHit        LazerHitResult = "small_tick_hit"
+	LazerLargeTickMiss       LazerHitResult = "large_tick_miss"
+	LazerLargeTickHit        LazerHitResult = "large_tick_hit"
+	LazerSmallBonus          LazerHitResult = "small_bonus"
+	LazerLargeBonus          LazerHitResult = "large_bonus"
+	LazerIgnoreMiss          LazerHitResult = "ignore_miss"
+	LazerIgnoreHit           LazerHitResult = "ignore_hit"
+	LazerComboBreak          LazerHitResult = "combo_break"
+	LazerSliderTailHit       LazerHitResult = "slider_tail_hit"
+	LazerLegacyComboIncrease LazerHitResult = "legacy_combo_increase"
 )
